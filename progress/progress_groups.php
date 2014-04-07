1d5a9634215c1fc3b2bd352de4cdc661e38e4fb6
Joy Youg <zhou.y.g890@gmail.com>
2014-04-07 21:56:10 +0800

    add group system

---
+++ progress/progress_groups.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Progress Groups</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/init.css" />
+		<link rel="stylesheet" type="text/css" href="css/event_group.css" />
+	</head>
+	<body>
+		<iframe id="headerIframe" src="header.html"></iframe>
+		<div id="groups_window">
+			<div id="groups_window_title">
+				<span id="groups_window_title_text"> Groups </span>
+			</div>
+			<div id="groups_window_types">
+				<ul>
+					<li class="groups_type_one">
+						你所在的组
+					</li>
+					<li class="groups_type_two">
+						<a href="#">浏览所有组</a>
+					</li>
+				</ul>
+			</div>
+			<div id="groups_window_list">
+				<ul>
+					<li class="list_group">
+						<div class="list_group_label">
+							<a href="#">Left 4 Dead</a>
+						</div>
+					</li>
+					<li class="list_group">
+						<div class="list_group_label">
+							<a href="#">Left 4 Dead 2</a>
+						</div>
+					</li>
+					<li class="list_group">
+						<div class="list_group_label">
+							<a href="#">Dota 2</a>
+						</div>
+					</li>
+					<li class="list_group">
+						<div class="list_group_label">
+							<a href="#">Dota 3</a>
+						</div>
+					</li>
+					<li class="list_group">
+						<div class="list_group_label">
+							<a href="#">Dota 4</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="jlibs/in.js"></script>
+</html>
